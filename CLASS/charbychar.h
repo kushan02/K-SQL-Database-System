ae7bc9743030681865e068216e55dba8e886bf43
Kushan <kushan.mehta02@gmail.com>
2016-11-12 14:52:57 +0530

    Final Version of cbyc Parsing logic
    This version is completely test and is working great with various command from all over net with no errors

--- CLASS/charbychar.h
+++ CLASS/charbychar.h
@@ -1,25 +1,25 @@
-  #include <iostream>
-
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
 #include <conio.h>
+
 using namespace std;
 
 
 char ar[50000];
-char sql[50][1000];
+char sql[50][10000];
 
 int line = 0, word = 0;
 
 int letter = 0;
-
 char ch;
 
 int i=0;
 
 bool sp = false;
+bool bracket = false;
+bool brin = false;
+bool qoute = false;
 
 void newline()
 {
@@ -102,11 +102,10 @@ bool enter()
 
 
 
-
 int main()
 {
 
-        cout<<" ksql> ";
+        cout<<endl<<" ksql> ";
         while(1)
         {
 
@@ -128,20 +127,29 @@ int main()
                                 continue;
                         }
                 }
-                else if((enter()) && sp == true)
+                else if((enter()) && sp == true && bracket==false && qoute == false)
                 {
+                        //  cout<<" * ";
                         cout<<endl<<"    -> ";
+
                         newline();
                         line--;
                         continue;
                 }
                 else if(enter())
                 {
+                        //    cout<<" ^ ";
+
                         if(i>0)
                         {
+                                //    cout<<" % ";
+
                                 cout<<endl<<"    -> ";
 
-                                newline();
+                                if(bracket == false && qoute == false)
+                                {
+                                        newline();
+                                }
 
                                 continue;
                         }
@@ -151,10 +159,103 @@ int main()
                 // TODO: BRACKET LOGIC HERE
                 /* Put the bracket in single line */
 
+                else if(ch == '(' && bracket == false && i==0)
+                {
+                        //    cout<<" # ";
+                        bracket = true;
+                        //    newline();
+                        sameline();
+                }
+                else if(ch == '(' && bracket == false && sql[line][i-1] != ' ')
+                {
+                        //    cout<<" @ ";
+                        bracket = true;
+                        newline();
+                        sameline();
+                }
 
+                else if(ch == '(' && bracket == false)
+                {
+                        //    cout<<" $ ";
+                        bracket = true;
+                        //  newline();
+                        sameline();
+                }
+
+                else if(ch == '(' && bracket == true)
+                {
+                        //    cout<<" % ";
+                        brin = true;
+                        sameline();
+                }
+                else if(ch == ')' && brin == true)
+                {
+                        //    cout<<" ^ ";
+                        brin = false;
+                        sameline();
+                }
+                else if(ch == ')' && bracket == true && brin == false)
+                {
+                        //    cout<<" & ";
+                        sameline();
+                        newline();
+                        bracket = false;
+                }
                 else if (extraspace())
                 {
                         continue;
+                }
+
+                else if(space() && i<1 && bracket == false && qoute == false)
+                {
+                        continue;
+                }
+                else if(extraspace() && bracket == true)
+                {
+                        continue;
+                }
+                else if(space() && i>1 && bracket == false && sql[line][i-1] == ',')
+                {
+                        sameline();
+                }
+                else if(sql[line][i-1] == ' ' && i>1 && bracket == false && qoute == false && ch == ',')
+                {
+                        sameline();
+                }
+                else if( (ch == '=' || ch == '>' || ch == '<' )  && strlen(sql[line])<1 /*&& i>0 */ && bracket==false && qoute == false)
+                {
+                        //    cout<<" * ";
+                        line--;
+                        i = strlen(sql[line]);
+                        sql[line][i++] = ' ';
+                        sameline();
+                }
+                else if( ch == ' ' && (sql[line][i-1] == '=' || sql[line][i-1] == '<' || sql[line][i-1] == '>') && bracket==false && qoute==false)
+                {
+                        //    cout<<" ! ";
+                        sameline();
+                }
+
+                else if(ch == '\'')
+                {
+                        if(qoute == false)
+                        {
+                                //  cout<<" @ ";
+                                qoute = true;
+
+                        }
+                        else if(qoute == true) {
+                                //  cout<<" # ";
+                                qoute = false;
+                        }
+                        sameline();
+
+                }
+
+                else if(bracket == true || qoute == true)
+                {
+                        //  cout <<" % ";
+                        sameline();
                 }
                 else if(space())
                 {
@@ -162,6 +263,7 @@ int main()
                         i--;
                         sameline();
                 }
+
                 else
                 {
                         sameline();
@@ -170,51 +272,11 @@ int main()
 
 
                 putchar(ch);
-
-                /*    else if(extraspace())
-                    {
-                            continue;
-                    }
-                    else if(enter())
-                    {
-
-                            cout<<endl<<"  -> ";
-
-                            newline();
-
-                            continue;
-
-                    }
-
-                    else if(isbackspace())
-                    {
-                            if(strlen(sql[line])!=0)
-                                    backspace();
-                    }
-
-                    else if(space())
-                    {
-                            newline();
-                            sameline();
-                    }
-                    else
-                    {
-
-                            sameline();
-                            letter++;
-                    }
-
-
-                    putchar(ch);
-
-
-                 */
         }
 
         sql[line][i]='\0';
 
-//    cout<<*p;
-//  cout<<endl<<ar;
+
         cout<<endl<<endl<<"-----------------"<<endl;
         for (size_t i = 0; i < line+1; i++) {
                 for (size_t j = 0; j < strlen(sql[i]); j++) {
@@ -224,5 +286,6 @@ int main()
                 cout<<endl;
                 /* code */
         }
+
         return 0;
 }
